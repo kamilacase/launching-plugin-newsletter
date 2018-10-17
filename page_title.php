49354a88679a290b0bad7d39c9e9d458eadd27b3
qrames <ramque@orange.fr>
2018-10-17 16:47:51 +0200

    code review

--- page_title.php
+++ page_title.php
@@ -4,12 +4,12 @@ class Zero_Page_Title
 {
     public function __construct()
     {
-    add_filter('document_title_parts', 'filter_title_part');
+        add_filter('document_title_parts', 'filter_title_part');
     }
     function filter_title_part($title) {
-    $title['title'] = 'zgeg';
-    return $title
-}
+        $title['title'] = 'zgeg';
+        return $title
+    }
 
 }
 ?>
